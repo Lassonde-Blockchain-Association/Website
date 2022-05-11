b438cb579d78043f9a8cd9bae33e428d848b8b6d
samsoncn <samsonc0514@gmail.com>
2022-05-11 13:39:28 -0400

    set up for Navbar workspace

---
+++ web-v1/src/components/Navbar/NavbarElements.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { Link as LinkR } from "react-router-dom";
+import { Link as LinkS } from "react-scroll";
+
+export const Nav = styled.nav``;
+
+export const NavbarContainer = styled.div``;
+
+export const NavLogo = styled(LinkR)``;
+
+export const MobileIcon = styled.div``;
+
+export const NavMenu = styled.ul``;
+
+export const NavItem = styled.li``;
+
+export const NavLinks = styled(LinkS)``;
+
+export const NavBtn = styled.nav``;
+export const NavBtnLink = styled(LinkR)``;
---
+++ web-v1/src/components/Navbar/index.js
@@ -0,0 +1,21 @@
+//rafce
+import React, { useState, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavLinks,
+  NavItem,
+  NavBtnLink,
+  NavBtn,
+} from "./NavbarElement";
+import { animateScroll as scroll } from "react-scroll";
+
+const Navbar = ({ toggle }) => {
+  return <></>;
+};
+
+export default Navbar;
