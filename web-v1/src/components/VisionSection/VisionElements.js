bc5eeb0cb9532376e112115a2d981b7cfe7380a6
James Chen <jchen9827@gmail.com>
2022-05-27 20:31:18 -0400

    Updated vision section structure, timeline content, and image folder

--- web-v1/src/components/VisionSection/VisionElements.js
+++ web-v1/src/components/VisionSection/VisionElements.js
@@ -2,13 +2,14 @@ import styled from "styled-components";
 
 export const VisionContainer = styled.div``;
 
-export const VisionContent = styled.div``;
-
 export const VisionH1 = styled.h1``;
 
 export const VisionH2 = styled.h2``;
 
-export const VisionP = styled.p``;
+export const VisionP = styled.p`
+    word-wrap: break-word;
+    display: flex;
+`;
 
 export const ValueWrapper = styled.div`
     // display: grid;
@@ -19,11 +20,22 @@ export const ValueWrapper = styled.div`
     justify-content: center;
 `;
 
-export const VisionRow = styled.div`
-    display: grid;
-    grid-auto-columns: minmax(auto, lfr);
+export const ValueRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
     align-items: center;
-    grid-template-areas: 'col1 col2 col3 col4';
+    // grid-auto-columns: minmax(auto, lfr);
+    // align-items: center;
+    // grid-template-areas: 'col1 col2 col3 col4';
+    @media only screen and (max-width: 600px){
+        flex-direction: column;
+    }
 `;
 
-export const Column1 = styled.div``;
+export const Column1 = styled.div`
+    display: flex;
+    flex-direction: column;
+    // max-width: 500px;
+`;
