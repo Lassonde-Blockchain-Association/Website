a647f890a12794b902fcf60563fdab756cbc332e
samsoncn <samsonc0514@gmail.com>
2022-07-18 09:09:32 +0800

    add team photo

--- web-v1/src/components/TeamSection/index.js
+++ web-v1/src/components/TeamSection/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import * as TeamElements from "./TeamElements";
 
-import img1 from "./images/img1.png";
+import img1 from "./images/img1.jpeg";
 import img2 from "./images/img2.jpeg";
 import img3 from "./images/img3.png";
 import img4 from "./images/img4.jpeg";
