ebd03234583195b300161f9a18c506fbee0f3fe1
James Chen <jchen9827@gmail.com>
2022-08-05 21:06:21 -0400

    Delete web-v1/docs directory

--- web-v1/docs/components/HomeSection/osMaptiler.js
+++
@@ -1,7 +0,0 @@
-export default {
-  maptiler: {
-    url: "https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=zZUlb8ZFjkDJErR48OpE",
-    attribution:
-      '&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-  },
-};
