5072479ec97ac1a23222772fb4515b83e33a93f5
milkdromeda <jchen9827@gmail.com>
2022-09-18 10:30:01 -0400

    Updated contact page with Discord and GForm

--- web-v1/src/components/Contact/index.js
+++ web-v1/src/components/Contact/index.js
@@ -29,6 +29,7 @@ import {
   FaYoutube,
   FaFacebook,
   FaInstagram,
+  FaPencilAlt,
 } from "react-icons/fa";
 
 const Contact = ({ imgStart, img }) => {
@@ -58,7 +59,24 @@ const Contact = ({ imgStart, img }) => {
                   </a>{" "}
                 </ContactLink>
 
-                <ContactH1>For Code/Networking Geeks</ContactH1>
+                <ContactH1>Joining Our Club</ContactH1>
+
+                <Text>
+                  Feel free to sign up to become a member via the Google Form
+                  below! It takes only 3 minutes to fill out and the information
+                  will help us make the most of our journey together. You can
+                  also stay up to date with us by joining our Discord server --
+                  non-official members are also welcome!
+                </Text>
+
+                <ContactLink>
+                  {" "}
+                  <a href="https://forms.gle/mA3g2T4SzV5JKrkbA">
+                    <FaPencilAlt /> Member Sign Up
+                  </a>{" "}
+                </ContactLink>
+
+                <ContactH1>For Code Geeks</ContactH1>
 
                 <Text>
                   If you would like take a backstage tour of our website, feel
@@ -79,40 +97,44 @@ const Contact = ({ imgStart, img }) => {
             {/* right side */}
             {/* find a img/drawing */}
             <Column2>
-              <InfoRow imgStart={imgStart}>
-                {/* <Column1> */}
-                <Circle>
-                  <SocialIcons>
-                    <SocialIconLink href="" target="" aria-label="Discord">
-                      <FaDiscord />
-                    </SocialIconLink>
-                  </SocialIcons>
-                </Circle>
-                <Circle>
-                  <SocialIcons>
-                    <SocialIconLink href="" target="" aria-label="Instagram">
-                      <FaInstagram />
-                    </SocialIconLink>
-                  </SocialIcons>
-                </Circle>
-                {/* </Column1> */}
-                {/* <Column2> */}
-                <Circle>
-                  <SocialIcons>
-                    <SocialIconLink href="" target="" aria-label="Facebook">
-                      <FaFacebook />
-                    </SocialIconLink>
-                  </SocialIcons>
-                </Circle>
-                <Circle>
-                  <SocialIcons>
-                    <SocialIconLink href="" target="" aria-label="YouTube">
-                      <FaYoutube />
-                    </SocialIconLink>
-                  </SocialIcons>
-                </Circle>
-                {/* </Column2> */}
-              </InfoRow>
+              {/* <a>
+                <ImgWrap>
+                  <Img src={img} />
+                </ImgWrap>
+              </a> */}
+              <SocialMediaRow>
+                <SocialMediaColumn>
+                  <Circle>
+                    <SocialIcons>
+                      <SocialIconLink href="https://discord.com/invite/JuCmutKUFM" target="" aria-label="Discord">
+                        <FaDiscord />
+                      </SocialIconLink>
+                    </SocialIcons>
+                  </Circle>
+                  <VerticalLine />
+                  <Circle>
+                    <SocialIcons>
+                      <SocialIconLink href="mailto:lassondeblockchain@gmail.com" target="" aria-label="Mail">
+                        <FaMailBulk />
+                      </SocialIconLink>
+                    </SocialIcons>
+                  </Circle>
+                </SocialMediaColumn>
+                <SocialMediaColumn>
+                  <div style={{marginTop:200}}></div>
+                  <HorizontalLine />
+                </SocialMediaColumn>
+                <SocialMediaColumn>
+                  <div style={{marginTop:200}}></div>
+                  <Circle>
+                    <SocialIcons>
+                      <SocialIconLink href="https://github.com/Lassonde-Blockchain-Club" target="" aria-label="GitHub">
+                        <FaGithub />
+                      </SocialIconLink>
+                    </SocialIcons>
+                  </Circle>
+                </SocialMediaColumn>
+              </SocialMediaRow>
             </Column2>
           </InfoRow>
         </ContactWrap>
