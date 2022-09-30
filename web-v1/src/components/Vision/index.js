5dd363aa5a2a14c285b0b44892d1006b008984a2
Razorlance <71834799+cthleung@users.noreply.github.com>
2022-09-29 21:10:01 -0400

    simplified vision section

--- web-v1/src/components/Vision/index.js
+++ web-v1/src/components/Vision/index.js
@@ -55,13 +55,13 @@ const Vision = () => {
           {/* <Background src={videoBg} autoPlay loop muted /> */}
           <VisionH1>Vision</VisionH1>
           <VisionStatement>
-            
-            We want to help make the world of Web3 more accessible to everyone. Through this project, our goal is to create intuitive, easily deployable data analytics and visualisation tools that help translate the arcane code contained in a blockchain transaction or NFT smart contract into something our users can more easily understand.<br></br>
+
+            We want to help make the world of Web3 more accessible to everyone. Our goal is to help make the complicated code contained in a blockchain transaction or NFT smart contract into something our users can more easily understand.<br></br>
             <br></br>
 
-            In order to achieve this, we will need enthusiastic talent on multiple fronts - including but not limited to UX/UI design, marketing, frontend/backend/API/blockchain development. This is an exciting time to learn about a burgeoning new field with limitless opportunities to change the world around us, and LBC offers a fantastic opportunity to gain this valuable early experience.
+            We're looking for enthusiastic talent on multiple fronts - including but not limited to UX/UI design, marketing, web/software development. This is an exciting time to learn about a exciting new field with limitless opportunities to change the world around us!
 
-            <br></br> <br></br> If you are interested in joining an ambitious team and contributing to innovative projects with real-world potential, feel free to get in touch with us!
+            <br></br> <br></br> If you are interested in joining an ambitious team and contributing to innovative projects with real-world potential, feel free to get in touch!
 
           </VisionStatement>
           <ProjectsWrapper>
