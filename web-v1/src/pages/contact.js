fa574f1e8e8cfe12d738a29c9dfb722c0cdc4663
samsoncn <samsonc0514@gmail.com>
2022-09-30 10:07:49 +0800

    update

--- web-v1/src/pages/contact.js
+++ web-v1/src/pages/contact.js
@@ -2,13 +2,11 @@ import React from "react";
 import Contact from "../components/Contact";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Home from "../components/Home";
 
 const ContactPage = () => {
   return (
     <>
       <Navbar />
-      {/* <Home /> */}
       <Contact />
       <Footer />
     </>
